a7b8c051e061251c330364acce0010e50197c12f
Jasonarea <hyeonsuns@naver.com>
2025-08-31 12:05:59 +0900

    Initial commit

---
+++ src/test/java/com/flab/vibeup/VibeUpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flab.vibeup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VibeUpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
