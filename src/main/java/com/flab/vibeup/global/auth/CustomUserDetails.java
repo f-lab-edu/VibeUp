c88a7d9c41d370d804ecd127f01693649c3b283e
Jasonarea <hyeonsuns@naver.com>
2025-09-29 20:46:16 +0900

    feat(auth): implement CustomUserDetails for authenticated user context(#12)

---
+++ src/main/java/com/flab/vibeup/global/auth/CustomUserDetails.java
@@ -0,0 +1,55 @@
+package com.flab.vibeup.global.auth;
+
+import com.flab.vibeup.domain.user.entity.User;
+import java.util.Collection;
+import java.util.Collections;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+@Getter
+@RequiredArgsConstructor
+public class CustomUserDetails implements UserDetails {
+
+    private final User user;
+
+    public Long getUserId() {
+        return user.getId();
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.emptyList(); // 권한 Empty 처리
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getEmail();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
