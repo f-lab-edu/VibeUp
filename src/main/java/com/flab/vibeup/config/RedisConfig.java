ee2ac6fa3ccd6e882724f85e8a41debadaf8df39
Jasonarea <hyeonsuns@naver.com>
2025-10-11 16:45:47 +0900

    feat: Redis 설정 및 Feed fan-out을 위한 RedisTemplate 기반 구조 추가(#22)

---
+++ src/main/java/com/flab/vibeup/config/RedisConfig.java
@@ -0,0 +1,24 @@
+package com.flab.vibeup.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+
+@Configuration
+public class RedisConfig {
+
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory() {
+        // localhost:6379
+        return new LettuceConnectionFactory();
+    }
+
+    @Bean
+    public RedisTemplate<String, Long> redisTemplate() {
+        RedisTemplate<String, Long> template = new RedisTemplate<>();
+        template.setConnectionFactory(redisConnectionFactory());
+        return template;
+    }
+}
---
+++ src/test/java/com/flab/vibeup/config/RedisConnectionTest.java
@@ -0,0 +1,21 @@
+package com.flab.vibeup.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+public class RedisConnectionTest {
+    @Autowired
+    private RedisTemplate<String, Long> redisTemplate;
+
+    @Test
+    void redisPushAndPullTest() {
+        redisTemplate.opsForList().leftPush("feed:1", 100L);
+        Long postId = redisTemplate.opsForList().leftPop("feed:1");
+        assertThat(postId).isEqualTo(100L);
+    }
+}
