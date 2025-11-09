e58ac847f44faa78f96bca122fd31327dce23f2f
Jasonarea <hyeonsuns@naver.com>
2025-11-09 12:19:11 +0900

    test: add Fan-out logic unit tests for PostService (followers & no followers case) (#23)

--- src/test/java/com/flab/vibeup/config/RedisConnectionTest.java
+++ src/test/java/com/flab/vibeup/config/RedisConnectionTest.java
@@ -1,14 +1,15 @@
 package com.flab.vibeup.config;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.redis.core.RedisTemplate;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
 @SpringBootTest
 public class RedisConnectionTest {
+
     @Autowired
     private RedisTemplate<String, Long> redisTemplate;
 
